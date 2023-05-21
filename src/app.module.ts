728d0aece2e38ad6431f02727c04c06552062e8b
cyanosj128 <cyanosj128@gmail.com>
2023-05-22 01:17:28 +0900

    feat: use typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from 'config';
+import { Stock } from './stock/stock.entity';
+import { Trade } from './trade/trade.entity';
+import { StockModule } from './stock/stock.module';
+import { TradeModule } from './trade/trade.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...typeOrmConfig,
+      type: 'mysql',
+      entities: [Stock, Trade],
+      synchronize: false, // false가 안전함
+    }),
+    StockModule,
+    TradeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
